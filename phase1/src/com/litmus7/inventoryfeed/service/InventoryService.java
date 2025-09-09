de156d55fd42a6cbebcfe25669e6ac57c8f47f9d
Rithas K <rithask@icloud.com>
2025-09-09 12:31:08 +0530

    refactor(phase 1): move file moving function to utils

--- phase1/src/com/litmus7/inventoryfeed/service/InventoryService.java
+++ phase1/src/com/litmus7/inventoryfeed/service/InventoryService.java
@@ -1,16 +1,11 @@
 package com.litmus7.inventoryfeed.service;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.util.List;
 
 import com.litmus7.inventoryfeed.constants.FileConstants;
 import com.litmus7.inventoryfeed.dao.InventoryDao;
 import com.litmus7.inventoryfeed.exceptions.CsvProcessingException;
-import com.litmus7.inventoryfeed.exceptions.FileProcessingException;
 import com.litmus7.inventoryfeed.model.Product;
 import com.litmus7.inventoryfeed.util.CsvUtils;
 import com.litmus7.inventoryfeed.util.FilesUtil;
@@ -39,21 +34,11 @@ public class InventoryService {
 			} catch (Exception e) {
 				throw new RuntimeException(e.getMessage());
 			}
+
 			if (filesAddedFlag) {
-				try {
-					Files.move(Paths.get(file.getPath()), Paths.get(FileConstants.PROCESSED_FOLDER + file.getName()),
-							StandardCopyOption.ATOMIC_MOVE);
-				} catch (IOException e) {
-					throw new FileProcessingException(
-							"Failed to move the file to the processed folder: " + e.getMessage());
-				}
+				FilesUtil.moveFile(file, FileConstants.PROCESSED_FOLDER);
 			} else {
-				try {
-					Files.move(Paths.get(file.getPath()), Paths.get(FileConstants.ERROR_FOLDER + file.getName()),
-							StandardCopyOption.ATOMIC_MOVE);
-				} catch (IOException e) {
-					throw new FileProcessingException("Failed to move the file to the error folder: " + e.getMessage());
-				}
+				FilesUtil.moveFile(file, FileConstants.ERROR_FOLDER);
 			}
 		}
 
--- phase1/src/com/litmus7/inventoryfeed/util/FilesUtil.java
+++ phase1/src/com/litmus7/inventoryfeed/util/FilesUtil.java
@@ -2,8 +2,14 @@ package com.litmus7.inventoryfeed.util;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 import com.litmus7.inventoryfeed.exceptions.CsvProcessingException;
+import com.litmus7.inventoryfeed.exceptions.FileProcessingException;
 
 public class FilesUtil {
 	public static File[] getCsvFiles(String folderPath) {
@@ -22,5 +28,16 @@ public class FilesUtil {
 		File[] files = directory.listFiles(filter);
 
 		return files;
+	}
+
+	public static void moveFile(File sourceFile, String destinationDir) {
+		Path destinationPath = Paths.get(destinationDir, sourceFile.getName());
+
+		try {
+			Files.move(sourceFile.toPath(), destinationPath, StandardCopyOption.ATOMIC_MOVE);
+		} catch (IOException e) {
+			throw new FileProcessingException(
+					"Failed to move file " + sourceFile.getName() + " to " + destinationDir + ": " + e.getMessage(), e);
+		}
 	}
 }
